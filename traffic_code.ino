fcf8d5cb7955c036d115c9635b227d87aa09d9bd
Iruno Lefort <irunolefort@gmail.com>
2025-02-11 19:47:31 +0100

    Update traffic_code.ino
    segments display connected

--- traffic_code.ino
+++ traffic_code.ino
@@ -18,15 +18,13 @@ const int lights[2][3] = {
 //Prepare binary array for all 7 segment to turn on 7 segment at position of a,b,c,d,e,f,g
 int digit[10] = {0b0111111, 0b0000110, 0b1011011, 0b1001111, 0b1100110, 0b1101101, 0b1111101, 0b0000111, 0b1111111, 0b1101111};
 
-const int controller_1h = 9;
-const int controller_2h = 10;
-const int controller_1v = 11;
-const int controller_2v = 12;
+//for all four segments display
+const int controller_1 = 9;
+const int controller_2 = 10;
 
 int digit1, digit2; // initialize individual digit to controll each segment
 
-int controllers1[2] = {controller_1h, controller_2h};
-int controllers2[2] = {controller_1v, controller_2v};
+int controllers[2] = {controller_1, controller_2};
 
 
 void setup() {
@@ -34,10 +32,8 @@ void setup() {
   {
     pinMode(i, OUTPUT); // declare 0-9 th pin as output
   }
-  pinMode(controller_1h, OUTPUT); //declare 7 seg Digit1_horizontal pin as output
-  pinMode(controller_2h, OUTPUT);//declare 7 seg Digit2_horizontal pin as output
-  pinMode(controller_1v, OUTPUT); //declare 7 seg Digit1_vertical pin as output
-  pinMode(controller_2v, OUTPUT);//declare 7 seg Digit2_vertical pin as output
+  pinMode(controller_1, OUTPUT); //declare 7 seg Digit1_horizontal pin as output
+  pinMode(controller_2, OUTPUT);//declare 7 seg Digit2_horizontal pin as output
   for (int i=0;i<2;i++){
     for(int j=0;j<3;j++){
       pinMode(lights[i][j], OUTPUT);
@@ -53,7 +49,7 @@ void loop(){
 }
 
 //counter
-void counter(int segment1[], int segment2[], int start_of_countdown, int end_of_countdown=0) {
+void counter(int segment[], int start_of_countdown, int end_of_countdown=0) {
   if(digitalRead(check)){
     for (int j = start_of_countdown; j>=end_of_countdown; j--)//
     {
@@ -61,16 +57,12 @@ void counter(int segment1[], int segment2[], int start_of_countdown, int end_of_
       digit1 = j % 10;
       for ( int k = 0; k<50; k++)//
       {
-        digitalWrite(segment1[0], HIGH);
-        digitalWrite(segment1[1], LOW);
-        digitalWrite(segment2[0], HIGH);
-        digitalWrite(segment2[1], LOW);
+        digitalWrite(segment[0], HIGH);
+        digitalWrite(segment[1], LOW);
         dis(digit1);
         delay(10);
-        digitalWrite(segment1[1], HIGH);
-        digitalWrite(segment1[0], LOW);
-        digitalWrite(segment2[1], HIGH);
-        digitalWrite(segment2[0], LOW);
+        digitalWrite(segment[1], HIGH);
+        digitalWrite(segment[0], LOW);
         dis(digit2);
         delay(10);
       }
@@ -125,22 +117,20 @@ void traffic_stop(){
         digitalWrite(lights[i][j], HIGH);
       }
     }
-  digitalWrite(controller_1v, HIGH);
-  digitalWrite(controller_2v, HIGH);
-  digitalWrite(controller_1h, HIGH);
-  digitalWrite(controller_2h, HIGH);
+  digitalWrite(controller_1, HIGH);
+  digitalWrite(controller_2, HIGH);
 }
 
 void traffic_light() {
   if(digitalRead(check)){
     green_on(lights[0], lights[1]);
-    counter(controllers1, controllers2, 10, 3);
+    counter(controllers, 10, 3);
     yellow_on(lights[0], lights[1]);
-    counter(controllers1, controllers2, 2, 0);
+    counter(controllers, 2, 0);
     green_on(lights[1], lights[0]);
-    counter(controllers2, controllers1, 5, 3);
+    counter(controllers, 5, 3);
     yellow_on(lights[1], lights[0]);
-    counter(controllers2, controllers1, 2, 0);
+    counter(controllers, 2, 0);
     }
   else{ 
     traffic_stop();
